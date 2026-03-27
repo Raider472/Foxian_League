141a277207a72cb6b89d400aef68fb5b5f309c88
Raider472 <gabrielnae1@gmail.com>
2026-03-27 15:18:46 +0100

    Vulyakko and rebalance Vulpano

---
+++ Source/Foxian_league/AnimalComps/CompProperties_vulyakkoResilience.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Foxian_league {
+    public class CompProperties_vulyakkoResilience : CompProperties {
+        //Comp prop for setting the chance of procing and how much to resist incoming damage
+        public float chanceToProc;
+        public float minPercentageReduction;
+        public float maxPercentageReduction;
+        public CompProperties_vulyakkoResilience() {
+            compClass = typeof(Comp_vulyakkoResilience);
+        }
+    }
+}
---
+++ Source/Foxian_league/AnimalComps/Comp_vulyakkoResilience.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace Foxian_league {
+    public class Comp_vulyakkoResilience : ThingComp {
+        //Comp to give a chance to Vulyakkos to reduce incoming damage by a percentage
+        public CompProperties_vulyakkoResilience Props => (CompProperties_vulyakkoResilience)props;
+
+        public override void PostPreApplyDamage(ref DamageInfo dinfo, out bool absorbed) {
+            base.PostPreApplyDamage(ref dinfo, out absorbed);
+            if(Rand.Chance(Props.chanceToProc)) {
+                float randomValue = (Rand.Value * (Props.maxPercentageReduction - Props.minPercentageReduction) + Props.minPercentageReduction);
+                Log.Message($"Ability has proced with damage = {dinfo.Amount} and was it absorbed ? {absorbed}");
+                int valueToReduce = Mathf.RoundToInt(dinfo.Amount * randomValue);
+                dinfo.SetAmount(dinfo.Amount - valueToReduce);
+                Log.Message($"New value set is: {dinfo.Amount} and reductin value was: {randomValue}");
+            }
+            else {
+                Log.Message($"damage postPreApply = {dinfo} and is absorbed ? {absorbed}");
+            }
+        }
+
+        public override void PostPostApplyDamage(DamageInfo dinfo, float totalDamageDealt) {
+            base.PostPostApplyDamage(dinfo, totalDamageDealt);
+            Log.Message($"damage postPostApply = {dinfo} and total damage = {totalDamageDealt}");
+        }
+    }
+}
