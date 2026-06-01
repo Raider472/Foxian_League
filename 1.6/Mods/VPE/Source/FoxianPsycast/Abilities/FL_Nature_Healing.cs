b33836245a342c46365cb184179a0ea02c1c2830
Raider472 <gabrielnae1@gmail.com>
2026-06-01 16:30:10 +0200

    AOE Heal + Clotting Class for Overclock

---
+++ 1.6/Mods/VPE/Source/FoxianPsycast/Abilities/FL_Nature_Healing.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace FoxianPsycast {
+    public class FL_Nature_Healing : VEF.Abilities.Ability {
+        public override void Cast(params GlobalTargetInfo[] targets) {
+            base.Cast(targets);
+            if(targets.Length == 0 || targets == null) return;
+            for(int i = 0; i < targets.Length; i++) {
+                Log.Message($"{targets[i].Pawn}");
+                List<Hediff_Injury> injuriesList = targets[i].Pawn.health.hediffSet.hediffs.OfType<Hediff_Injury>().ToList();
+                Log.Message($"Injuries found: {injuriesList.Count}");
+                if(injuriesList.Any()) {
+                    HealInjuries(injuriesList);
+                }
+                else {
+                    Log.Message("No injuries to heal");
+                    continue;
+                }
+            }
+        }
+
+        private void HealInjuries(List<Hediff_Injury> injuries) {
+            foreach(Hediff_Injury injury in injuries) {
+                if (injury.IsPermanent()) continue;
+                float skipHeal = Rand.Value;
+                if(skipHeal < 0.2f) {
+                    Log.Message($"Skipping healing for {injury.Label} with severity {injury.Severity}");
+                    continue;
+                }
+                Log.Message($"{injury.Label} and severity {injury.Severity}");
+                float randomHealValue = (Rand.Value * (injury.Severity - (injury.Severity * 0.01f)) + (injury.Severity * 0.01f));
+                injury.Heal(randomHealValue);
+                Log.Message($"Healed {randomHealValue} point of {injury.Label}, new severity: {injury.Severity}");
+            }
+        }
+    }
+}
